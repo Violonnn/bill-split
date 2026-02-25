d5ec9dd2696adcb83668ca52300d4a0b09ad380d
Violonnn <mstevenviolon@gmail.com>
2026-02-25 09:50:14 +0800

    first commit :)

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express'  
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express() 
+
+// Middlewares
+app.use(cors())
+app.use(express.json())
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Bill Split API Running 🚀')
+})
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+// Start server
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
